602746bf22f11e0e1c2bf4b3314bb6b3bd4459df
ropu <puegner@chemmedia.de>
2016-01-30 22:29:21 +0100

    tab corrections

--- hallled/api/hall_led.py
+++ hallled/api/hall_led.py
@@ -1,9 +1,6 @@
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
-
-import serial
 import logging
+import serial
+from pyramid.response import Response
 logger = logging.getLogger("pyramid.app.hall_led")
 logger.setLevel(logging.DEBUG)
 
@@ -33,13 +30,3 @@ def led(command, operator, amount):
 	ser.flushInput()
 	ser.close()
 	return serResponse
-
-if __name__ == '__main__':
-    config = Configurator()
-    config.add_route('hello', '/hello/{name}')
-    config.add_view(hello_world, route_name='hello')
-    config.add_route('api', '/api/led/{command}/{operator}/{amount}')
-    config.add_view(api_led, route_name='api')
-    app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', 8081, app)
-    server.serve_forever()
