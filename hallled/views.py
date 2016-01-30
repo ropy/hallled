83c30021d3717b14713ed22b239d78567e4421ff
ropu <puegner@chemmedia.de>
2016-01-30 23:02:43 +0100

    removed annotation

--- hallled/views.py
+++ hallled/views.py
@@ -3,7 +3,6 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'HallLed'}
-    
-@view_config(renderer='json')
+
 def api_led(request):
 	return Response('OK')
